bd9ffde9191ba740ce9718b3567ab2f8735b90d2
vitalight <904844050@qq.com>
2018-12-25 16:04:25 +0800

    Add button to support restart

---
+++ Assets/Beaker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beaker : MonoBehaviour {
+
+    Vector3 cubeStartPosition;
+    Quaternion cubeStartRotation;
+    
+	void Start ()
+    {
+        var cube = transform.Find("Cube").transform;
+        cubeStartPosition = cube.localPosition;
+        cubeStartRotation = cube.localRotation;
+	}
+	
+    public void ResetCubeTransform()
+    {
+        var cube = transform.Find("Cube").transform;
+        cube.localPosition = cubeStartPosition;
+        cube.localRotation = cubeStartRotation;
+    }
+}
